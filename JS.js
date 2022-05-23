1b9c5a86c73554b2425ae77e974aa078d2badbf9
Mashalane Pheletso <89118208+PheletsoBear@users.noreply.github.com>
2022-05-23 11:53:34 +0200

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,9 @@
+
+window.alert("this is a alert!!!");
+document.getElementById("click").innerHTML="this is a JS text";
+function myClickFunction()
+{
+       document.getElementById("text").innerHTML="THE SECOND VERSION OF THE TEXT";                
+
+}
+
